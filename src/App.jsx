bcd7eb56a007705fcd179bb693288a63a9b07457
matubruno16 <matubruno16@gmail.com>
2024-01-31 19:08:15 -0300

    Vercel Web Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import NavBar from "./components/NavBar"
 import { SeccionDivisas } from "./components/SeccionDivisas"
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <main className=" min-h-dvh min-w-full ">
     <SpeedInsights/>
+    <Analytics />
     <h1 className="hidden">
     Cotización del Dólar y Euro en Tiempo Real - Argentina
     </h1>
