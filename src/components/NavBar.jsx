e47f4b95d993ccf08d93d07fb6356c39873fc74b
matubruno16 <matubruno16@gmail.com>
2024-02-06 10:18:34 -0300

    Correcciones Nabvar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import logoDolarHoy from "../assets/dolarHoy.png";
 import { IconMenu } from "./IconMenu";
+import { Link } from "react-router-dom";
+
 
 
 const NavBar = () => {
@@ -11,22 +13,21 @@ const NavBar = () => {
     setMenuOpen(!isMenuOpen);
   };
 
-  const links = [
-    { link: "Dólar Oficial", id: 1 },
-    { link: "Dólar Blue", id: 2 },
-    { link: "Dólar Turista", id: 3 },
-    { link: "Otras Divisas", id: 4 },
-  ];
+  const classLinkIsOpen = "max-w-max font-semibold text-primario text-xl text-4xl mt-6 hover:cursor-pointer hover:scale-110 transition-all delay-100 duration-200 ease-in-out uppercase ml-3 lg:mx-3 xl:mx-5 ";
+
+  const classLinkNotOpen = "max-w-max flex items-center justify-center text-primario font-semibold text-2xl text-left hover:cursor-pointer hover:scale-110 transition-all delay-100 duration-300 uppercase mx-6 ";
 
   return (
     <nav
-      className={`  ${!isMenuOpen ? "flex w-screen p-3 justify-between md:justify-around bg-divisa backdrop-blur-sm items-center justify-items-center px-6 pl-6 " : " fixed h-full w-full  bg-divisa flex flex-col justify-start pl-6 pt-6"}`}
+      className={`  ${!isMenuOpen ? "w-full flex p-3 justify-between md:justify-around bg-divisa backdrop-blur-sm items-center justify-items-center px-6 pl-6 " : " fixed  h-full w-full  bg-divisa flex flex-col justify-start px-6 pt-6"}`}
     >
+      <div className={`${!isMenuOpen ? "w-full flex items-center justify-between" : "flex items-center justify-between"}`}>
 
-      <a 
-        className={`${isMenuOpen ? "hidden" : ""} `}       
-        title="Dolar Hoy- Cotizaciones del dia. Dolar / Euro"
-        href="/">
+
+        <a
+          className={`${isMenuOpen ? "" : ""} `}
+          title="Dolar Hoy- Cotizaciones del dia. Dolar / Euro"
+          href="/">
           <img
             src={logoDolarHoy}
             alt="Icono Dolar Hoy"
@@ -34,49 +35,31 @@ const NavBar = () => {
             height={40}
           />
 
-      </a>
-      <div className={`${isMenuOpen ? "flex md:hidden pb-10" : "md:hidden"}`}>
+        </a>
+        <div className={`${isMenuOpen ? "flex md:hidden " : "md:hidden"}`}>
 
-        <IconMenu
-          handleMenuClick={handleMenuClick}
-          
-        />
+          <IconMenu
+            handleMenuClick={handleMenuClick}
+
+          />
+        </div>
       </div>
-
-      <div className="hidden md:inline-flex text-md">
-
-        {!isMenuOpen &&
-          links.map((l) => (
-            <a
-              onClick={() => setMenuOpen(false)}
-              className=" max-w-max font-semibold text-primario text-xl lg:text-2xl  hover:cursor-pointer hover:scale-110 transition-all delay-100 duration-200 ease-in-out uppercase ml-3 lg:mx-3 xl:mx-5 "
-              to={l.link}
-              key={l.id}
-              href="/"
-              title={l.link}
-            >
-              {l.link}
-            </a>
-          ))}
+      <div className={`${!isMenuOpen ? "md:flex hidden" : "md:hidden flex flex-col "}`}>
+        <Link
+          to="/"
+          title="Inicio cotizaciones"
+          onClick={() => setMenuOpen(false)}
+          className={`${isMenuOpen ? classLinkIsOpen : classLinkNotOpen}`}
+        >Cotizaciones
+        </Link>
+        <Link
+          to="calculadora"
+          title="Calculadora"
+          onClick={() => setMenuOpen(false)}
+          className={`${isMenuOpen ? classLinkIsOpen : classLinkNotOpen}`}
+        >Calculadora
+        </Link>
       </div>
-
-      {isMenuOpen &&
-        links.map((l) => (
-          <a
-            onClick={() => setMenuOpen(false)}
-            className="max-w-max text-primario text-3xl text-left hover:cursor-pointer hover:scale-110 transition-all delay-100 duration-200 uppercase py-10 "
-            to={l.link}
-            key={l.id}
-            href="/"
-            title={l.link}
-          >
-            {l.link}
-          </a>
-        ))}
-
-
-
-
     </nav>
   );
 };
